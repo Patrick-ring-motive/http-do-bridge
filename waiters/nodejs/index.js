95f6e566cd1ba80e5bd89a4632fa4026f986d12a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-09 21:47:28 -0500

    Update index.js

--- waiters/nodejs/index.js
+++ waiters/nodejs/index.js
@@ -1,52 +1,31 @@
-const BRIDGE = process.env.BRIDGE ??
-	"https://example.com/bridge";
-
-const sleep = ms =>
-	new Promise(resolve => setTimeout(resolve, ms));
-
+const BRIDGE = process.env.BRIDGE ?? "https://example.com/bridge";
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const processRequest = async item => {
-	console.log("received", item.transaction_id);
-
-	/*
-	 * Do whatever the waiter is supposed to do.
-	 */
-	const response = [...new Set(item.payload)].join("");
-
-	return {
-		transaction_id: item.transaction_id,
-		response
-	};
+  console.log("received", item.transaction_id);
+  /*
+   * Do whatever the waiter is supposed to do.
+   */
+  const response = [...new Set(item.payload)].join("");
+  return {
+    transaction_id: item.transaction_id,
+    response
+  };
 };
-
 const listen = async () => {
-	while (true) {
-		try {
-			const res = await fetch(`${BRIDGE}/listen`);
-            (async()=>{
-			if (!res.ok) {
-				console.warn(
-					"listen:",
-					res.status,
-					await res.text()
-				);
-			}
-
-			const item = await res.json();
-
-			const response = await processRequest(item);
-
-			await fetch(`${BRIDGE}/response`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify([response])
-			});
-			})().catch(console.warn):
-		} catch (e) {
-			console.warn("waiter:", e);
-		}
-	}
+  while (true) {
+    try {
+      const res = await fetch(`${BRIDGE}/listen`);
+      (async () => {
+        const item = await res.json();
+        const response = await processRequest(item);
+        await fetch(`${BRIDGE}/response`, {
+          method: "POST",
+          body: JSON.stringify([response])
+        });
+      })().catch(console.warn);
+    } catch (e) {
+      console.warn("waiter:", e);
+    }
+  }
 };
-
 listen().catch(console.error);
